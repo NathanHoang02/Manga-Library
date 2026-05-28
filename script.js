842a351b4c4b9f148ec07d7d45b6aab68cd9247a
Nathan Hoang <nhoang6@lsu.edu>
2026-05-28 15:21:10 -0500

    Added dialog for adding manga

---
+++ script.js
@@ -0,0 +1,13 @@
+const addManga = document.querySelector("#add-manga");
+const close = document.querySelector(".modal-close");
+const dialog = document.querySelector("#add-manga-modal");
+
+addManga.addEventListener("click", () => 
+  {
+  dialog.showModal();
+});
+
+close.addEventListener("click", () => 
+  {
+  dialog.close();
+});
